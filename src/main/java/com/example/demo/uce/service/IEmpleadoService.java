d62b1493c930aac10c3b7998391188e55e32e5a1
Anthonny Montero <heatkdwow@gmail.com>
2022-08-17 19:48:26 -0500

    1708Push

--- src/main/java/com/example/demo/uce/service/IEmpleadoService.java
+++ src/main/java/com/example/demo/uce/service/IEmpleadoService.java
@@ -1,5 +1,10 @@
 package com.example.demo.uce.service;
 
-public interface IEmpleadoService {
+import com.example.demo.uce.repository.model.Empleado;
 
+public interface IEmpleadoService {
+	public void crear(Empleado emp);
+	public void actualizar(Empleado emp); 
+	public void elimiar(Integer id); 
+	public Empleado buscarPorId(Integer id); 
 }
